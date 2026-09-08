761ec1aff68a99ef43a0bff2d017de5903519e1e
Randy Idaeho <timeoso@outlook.com>
2026-09-08 18:05:27 +0100

    refactor: use Supabase publishable key in client

--- packages/app/app/supabase-client.js
+++ packages/app/app/supabase-client.js
@@ -1,2 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const publishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const supabase = createClient(url, publishableKey);
